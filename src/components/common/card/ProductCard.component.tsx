1ad19b31e6a7462c1caaf3e8d53c27784a48aedd
Martin Ivanko <martinivanko1238@gmail.com>
2022-08-20 14:16:23 +0200

    edit card

--- src/components/common/card/ProductCard.component.tsx
+++ src/components/common/card/ProductCard.component.tsx
@@ -1,19 +1,14 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import ArrowRightAltOutlinedIcon from "@mui/icons-material/ArrowRightAltOutlined";
+import Link from "@/common/link";
 
 interface ExpandMoreProps extends IconButtonProps {
     expand: boolean;
@@ -54,11 +49,12 @@ export default function ProductCard() {
             </CardContent>
             <CardActions disableSpacing>
                 <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
+                    <ShoppingCartOutlinedIcon />
                 </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon />
-                </IconButton>
+                <Link href="/">
+                    Detail
+                    <ArrowRightAltOutlinedIcon />
+                </Link>
             </CardActions>
         </Card>
     );
